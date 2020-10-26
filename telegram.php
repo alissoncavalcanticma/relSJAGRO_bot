68b904845d91d6fdeb5d14e7ba08aee111c2364b
Alisson C <alissoncavalcanticma@gmail.com>
2020-10-26 00:40:16 -0300

    dev flow

--- telegram.php
+++ telegram.php
@@ -6,61 +6,108 @@ $app = new \PhBotLib\Api\ApiConnectTelegram([
     'token' => '1319527547:AAH3hLsu7SwanQl6SWdYkRhaKTeYwJ_YG8Y'
 ]);
     
-    /*
-    while (true) {
+/*
+    $request = $app->getUpdates('offset = NULL');
 
-
-        $request = $app->getUpdates();
+    $xml = json_decode(json_encode($request),true);
+    
+    $chat_id = $xml['0']['message']['chat']['id'];
+    */
+    do {
+        //sleep(1); // Determina que a próxima iteração sera feita daqui a 60 segundos
+        $request = $app->getUpdates('offset = NULL');
 
         $xml = json_decode(json_encode($request),true);
 
-        if($xml[3]['message']['text'] == 'oi'){
-            $data = [
-                'chat_id' => 1166641089,
-                'text'    => 'Text!'
-            ];
-            $app->sendMessage($data);
+        $length = count($xml);
+
+
+        if($xml[$length-1]['update_id'] !== $update_id){
+            $update_id = $xml[$length-1]['update_id'];
+            $text = strtoupper($xml[$length-1]['message']['text']);
+
+            switch ($text) {
+                case "OI":
+                    $data = [
+                        'chat_id' => 1166641089,
+                        'parse_mode' => 'HTML',
+                        'text'    => "Olá, meu nome é <b>bot!</b>"
+                        //'text'    => "Olá, meu nome é <b>bot</b>\nEscolha uma opção:\n1- teste 1\n2 - teste 2"
+                        
+                    ];
+                    $app->sendMessage($data);
+                    /*
+                    do {
+                        $app->sendMessage($data);
+
+                        $request = $app->getUpdates('offset = NULL');
+                        //$obj = json_decode($request);
+
+                        $xml = json_decode(json_encode($request),true);
+
+                        $length = count($xml);
+
+
+                        if($xml[$length-1]['update_id'] === $update_id){
+                            $update_id = $xml[$length-1]['update_id'];
+                            $text = $xml[$length-1]['message']['text'];
+                            switch ($text) {
+                                case "UM":
+                                    echo "oi"; die();
+                                    $data = [
+                                        'chat_id' => $chat_id,
+                                        'text'    => 'Escolheu teste 1'
+                                    ];
+                                    $app->sendMessage($data);
+                                    break;
+                                default:
+                                    # code...
+                                    break;
+                            }
+                            
+                        }die();
+                    } while (true);
+                    */
+                    break;
+                case "TUDO BEM?":
+                    $data = [
+                        'chat_id' => 1166641089,
+                        'text'    => 'Tudo sim, e você?'
+                    ];
+                    $app->sendMessage($data);
+                    break;
+                case "TUDO CERTO":
+                    $data = [
+                        'chat_id' => 1166641089,
+                        'text'    => 'Que bom! Em que posso te ajudar?'
+                    ];
+                    $app->sendMessage($data);
+                    break;
+                case "ESTOU COM FOME!":
+                    $data = [
+                        'chat_id' => 1166641089,
+                        'text'    => 'Você é casado, come sua mulher! kkkkk'
+                    ];
+                    $app->sendMessage($data);
+                    break;
+                default:
+                    $data = [
+                        'chat_id' => 1166641089,
+                        'text'    => 'Comando inválido, repita por favor!'
+                    ];
+                    $app->sendMessage($data);
+                    break;
+            }
+        }else{
+            echo "Erro!";
+            //return $update_id = '0';
+
+        }
+    } while (true);
     
-        };
-        sleep(200); // Determina que a próxima iteração sera feita daqui a 60 segundos
-    }
-    */
 
-    $request = $app->getUpdates();
-    //$obj = json_decode($request);
 
-    $xml = json_decode(json_encode($request),true);
-
-    $length = count($xml);
-
-    //obtém a última atualização recebida pelo bot
-    $update = $xml[$length-1]['message']['text'];
-
-    //echo $update;
-    if($update === 'oi'){
-        $data = [
-            'chat_id' => 1166641089,
-            'text'    => 'Olá, meu nome é bot!'
-        ];
-        $app->sendMessage($data);
-    }else{
-        $data = [
-            'chat_id' => 1166641089,
-            'text'    => 'Não Entendi, pode repetir?'
-        ];
-        $app->sendMessage($data);
-
-    }
-    //$request = $app->getUpdates();
-    //var_dump($request['0']['result']['']);
-    //var_dump($request);
-    //$xml = json_decode(json_encode($request),true);
-    //echo $xml[0]['update_id'];
-    /*foreach($xml['message']['text'] as $text){
-        echo $text;
-    }
-    */
-    /*
+/*  
     $updates = getUpdateOffset();
 
         if($updates) {
@@ -86,41 +133,6 @@ $app = new \PhBotLib\Api\ApiConnectTelegram([
         }
 
 
-        */
-    /*
-    
-    echo $xml[3]['message']['text'];
+*/
 
-    for($x = 1; $x <= count($xml); $x++){
-        echo $xml[$x]['message']['text']."<br>";
-    }
-    
-    if($xml[3]['message']['text'] == 'oi'){
-        $data = [
-            'chat_id' => 1166641089,
-            'text'    => 'Text!'
-        ];
-        //$app->sendMessage($data);
-
-    };
-} catch (\Exception $exception) {
-    echo $exception->getMessage();
-}
-
-//json_decode($request, true);
-//json_decode($request, true)['result'];
-
-/*
-$data = [
-    'chat_id' => 1166641089,
-    'text'    => 'Ó a mensagem'
-];
-
-try {
-    $request = $app->sendMessage($data);
-} catch (\Exception $exception) {
-    echo $exception->getMessage();
-}
-//var_dump($request);
- */
 ?>
