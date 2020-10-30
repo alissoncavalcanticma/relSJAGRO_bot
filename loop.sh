9c539f9c45fee659d96891e3b79345465449da6d
Alisson C <alissoncavalcanticma@gmail.com>
2020-10-29 22:56:45 -0300

    organizaçao dos arquivos e identaçao de code

--- loop.sh
+++
@@ -1,1 +0,0 @@
-while true ; do echo telegran.php  & sleep 2; done
--- telegram.php
+++ telegram.php
@@ -1,6 +1,7 @@
 <?php
 
 require __DIR__ . "/vendor/autoload.php";
+require 'functions.php';
 
 //Add at the top this in your code Unlimited execution time
 
@@ -18,93 +19,16 @@ define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 //Inativar webhook
 //https://api.telegram.org/bot1319527547:AAH3hLsu7SwanQl6SWdYkRhaKTeYwJ_YG8Y/setwebhook?url=
 
-function getResult($rel, $title){
 
-	$out = "Resultado - ".$title;
-
-	if($rel=="RELGER"){
-		$out .= 'Relatório de entrada geral
-        26/10/2020
-        ================
-        TC Analisada : 5686.41
-        ================
-        TC Entregue : 6908.78
-        ================
-        Porcentagem de TC analisada : 82.31%
-        ================
-        Total de cargas entregues : 459
-        ================
-        Frente 1
-        
-        POLCANA : 10.91
-        AR : 1.12
-        FIBRA : 15.46
-        PUREZA : 80.59
-        BRIX : 17.48
-        ATR : 110.79
-        TMP: 51.39
-        ================';
-	}else{
-		$out .= 'Não existe';
-	}
-
-	return $out;
-}
-
-function processMessage($message) {
-    // processa a mensagem recebida
-    $message_id = $message['message_id'];
-    $chat_id = $message['chat']['id'];
-    if (isset($message['text'])) {
-      
-      $text = $message['text'];//texto recebido na mensagem
   
-      if (strpos($text, "/start") === 0) {
-          //envia a mensagem ao usuário
-          //Criar função pra verificar e retornar o ID para a pessoas solicitar o acesso às funções do BOT
-        sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Olá, '. $message['from']['first_name'].
-          '! Eu sou um bot de relatórios da São José. Para começar, escolha qual Relatório você deseja ver:', 'reply_markup' => array(
-          'keyboard' => array(array('Relatório Geral', 'Relatório por frente'),array('Relatório do dia anterior','Relatório XYZ'),array('Relatório 11','Relatório 12')),
-          'one_time_keyboard' => true)));
-      } else if ($text === "Relatório Geral") {
-        sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getResult('RELGER', $text)));
-      } else if ($text === "Relatório por frente") {
-        sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getResult('RELF', $text)));
-      } else if ($text === "Relatório do dia anterior") {
-        sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getResult('RELDA', $text)));
-      } else if ($text === "Relatório XYZ") {
-        sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getResult('RELXYZ', $text)));
-      } else {
-        sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Desculpe, mas não entendi essa mensagem. :('));
-      }
-    } else {
-      sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Desculpe, mas só compreendo mensagens em texto'));
-    }
-  }
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
 
+if (isset($update["message"])) {
 
-  function sendMessage($method, $parameters) {
-       
-        $options = array(
-        'http' => array(
-        'method'  => 'POST',
-        'content' => json_encode($parameters),
-        'header'=>  "Content-Type: application/json\r\n" .
-                    "Accept: application/json\r\n"
-        )
-        );
-  
-        $context  = stream_context_create( $options );
-        file_get_contents(API_URL.$method, false, $context );
-  }
-  
-    $update_response = file_get_contents("php://input");
-
-    $update = json_decode($update_response, true);
-
-    if (isset($update["message"])) {
     processMessage($update["message"]);
-    }
+    
+}
 
 
   //obtém as atualizações do bot
